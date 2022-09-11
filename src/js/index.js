ad264e80229115d0287849b2718493ba0ecf9684
Mateusz Meler <mateuszmeler@icloud.com>
2022-09-11 02:56:59 +0200

    adding a variables

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,22 @@ import "../scss/main.scss";
 
 /* place your code below */
 console.log(`Everything's fine !`);
+const total = document.getElementById("total");
+const charging = document.getElementById("charging");
+let mileage = document.querySelector(".mileage");
+mileage.textContent = 0;
+
+charging.addEventListener("change", (e) => {
+  const chargingValue = e.target.value;
+  total.addEventListener("change", (e) => {
+    const totalValue = e.target.value;
+    if (chargingValue && totalValue) {
+      mileage = totalValue - chargingValue;
+      mileage = mileage.toFixed(1) * 1;
+      console.log(mileage);
+      document.querySelector(".mileage").innerHTML = mileage;
+    } else {
+      document.querySelector(".mileage").innerHTML = 0;
+    }
+  });
+});
