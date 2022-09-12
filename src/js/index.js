3e9b8f93634ff93e6ef41ec69ddc25c204eda993
Mateusz Meler <mateuszmeler@icloud.com>
2022-09-12 19:14:47 +0200

    js update

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-// import { registerSW } from "./pwa.js";
-// registerSW();
+import { registerSW } from "./pwa.js";
+registerSW();
 
 /* place your code below */
 let total = document.getElementById("total");
@@ -11,45 +11,55 @@ let mileage = document.querySelector(".mileage");
 let drive = document.querySelector(".drive");
 const save = document.querySelector(".save");
 const load = document.querySelector(".load");
-let totalStorage = "";
-let chargingStorage = "";
-mileage.textContent = 0;
-drive.textContent = 0;
+let totalStorage = 0;
+let chargingStorage = 0;
 
-charging.addEventListener("change", (e) => {
-  const chargingValue = e.target.value;
+document.querySelector(".mileage").innerHTML = 0;
+document.querySelector(".drive").innerHTML = 0;
 
-  total.addEventListener("change", (e) => {
-    const totalValue = e.target.value;
-    if (chargingValue && totalValue) {
+charging.addEventListener("input", (e) => {
+  let chargingValue = e.target.value;
+
+  total.addEventListener("input", (e) => {
+    let totalValue = e.target.value;
+
+    if (chargingValue > 0 && totalValue > 0) {
       mileage = totalValue - chargingValue;
       mileage = mileage.toFixed(1) * 1;
       document.querySelector(".mileage").innerHTML = mileage;
-    } else {
-      document.querySelector(".mileage").innerHTML = 0;
+      console.log(typeof mileage);
     }
     if (mileage <= 41 && mileage >= 0) {
       drive = 41 - mileage;
       drive = drive.toFixed(1) * 1;
       document.querySelector(".drive").innerHTML = drive;
-      if (drive <= 5) {
-        document.querySelector(".drive").classList.add("red");
-      } else {
-        document.querySelector(".drive").classList.remove("red");
-      }
+    } else {
+      document.querySelector(".mileage").innerHTML = 0;
+      document.querySelector(".drive").innerHTML = 0;
     }
+
+    if (drive <= 5) {
+      document.querySelector(".drive").classList.add("red");
+    } else {
+      document.querySelector(".drive").classList.remove("red");
+    }
+
+    save.addEventListener("click", () => {
+      if (total.value && charging.value) {
+        localStorage.setItem("total", total.value);
+        localStorage.setItem("charging", charging.value);
+        console.log("masz coś w storage");
+      }
+    });
+    load.addEventListener("click", () => {
+      totalStorage = localStorage.getItem("total");
+      chargingStorage = localStorage.getItem("charging");
+      let jSon = JSON.parse(totalStorage);
+      let jSon2 = JSON.parse(chargingStorage);
+      total.value = jSon;
+      charging.value = jSon2;
+    });
   });
 });
-save.addEventListener("click", () => {
-  if (total.value && charging.value) {
-    localStorage.setItem("total", total.value);
-    localStorage.setItem("charging", charging.value);
-    console.log("masz coś w storage");
-  }
-});
-load.addEventListener("click", () => {
-  totalStorage = localStorage.getItem("total");
-  chargingStorage = localStorage.getItem("charging");
-  total.value = totalStorage;
-  charging.value = chargingStorage;
-});
+
+// console.log(`working !`);
